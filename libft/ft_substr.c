63178cd8facd123dcf49061e30d338b3c7ae07a7
viru <lavirgingmail.com>
2025-05-04 08:13:28 +0000

    substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: virginia <virginia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 18:28:00 by virginia          #+#    #+#             */
-/*   Updated: 2025/05/04 07:10:00 by virginia         ###   ########.fr       */
+/*   Updated: 2025/05/04 08:13:01 by virginia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
+	size_t	i;
 
 	substr = malloc(len + 1);
-	if (!substr)
+	if (!substr || start > (unsigned int)ft_strlen(s))
 		return (NULL);
+	i = 0;
 	while (s[start] && len)
 	{
-		substr[start] = s[start];
+		substr[i] = s[start];
 		start++;
+		i++;
 		len--;
 	}
-	substr[start] = '\0';
+	substr[i] = '\0';
+	if (len > (size_t)(ft_strlen(substr)+1))
+		free(&substr[i+1]);
 	return (substr);
 }
