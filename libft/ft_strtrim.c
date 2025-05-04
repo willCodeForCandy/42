080521ffea77d99bfd890d9e8cf5defe9d875e60
viru <lavirgingmail.com>
2025-05-04 16:58:06 +0000

    fix strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: virginia <virginia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 09:46:39 by virginia          #+#    #+#             */
-/*   Updated: 2025/05/04 16:41:37 by virginia         ###   ########.fr       */
+/*   Updated: 2025/05/04 16:55:03 by virginia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char *ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 
 	if(!s1 || !set)
-		return (s1);
+		return ((char *)s1);
 	start = 0;
 	end = ft_strlen(s1);
-	while (ft_strchr(set[start], (int)*s1))
+	while (ft_strchr(set, (int)s1[start]))
 		start++;
-	while (ft_strrchr(set[end], (int)*s1))
+	while (ft_strrchr(set, (int)s1[end]))
 		end--;
 	trimmed_str = ft_substr(s1, (unsigned int)start, end - start + 1);
 	return (trimmed_str);
